d20ba62d493e3566d6461e14a40d6bc0752e2348
RAZGRIZ219104 <razgriz219104@gmail.com>
2024-12-20 19:27:15 +0100

    fix: update .gitignore to dist folder

--- dist/@types/i18next/resources.js
+++
@@ -1,8 +0,0 @@
-// import enTranslation from "../../locale/en.json";
-// import jpTranslation from "../../locale/jp.json";
-// const resources = {
-//   enTranslation,
-//   jpTranslation,
-// } as const;
-// export default resources;
-//# sourceMappingURL=resources.js.map
